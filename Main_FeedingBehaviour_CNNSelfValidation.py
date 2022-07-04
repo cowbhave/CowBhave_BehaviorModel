c13c38ed37ce59ecf2e5a95794cba3852adfd060
Victor Bloch <47314151+VicB18@users.noreply.github.com>
2022-07-04 11:48:59 +0300

    Add files via upload

--- Main_FeedingBehaviour_CNNSelfValidation.py
+++ Main_FeedingBehaviour_CNNSelfValidation.py
@@ -1,7 +1,3 @@
-# .\venv\Scripts\activate
-# cd C:\Users\03138529\Dropbox\Luke\CowBhave
-# python Main_FeedingBehaviour_CNNSelfValidation.py
-
 from FeedingBehavior_NNlib import *
 import fnmatch, os
 from sklearn.model_selection import train_test_split
@@ -14,86 +10,84 @@ from datetime import datetime
 import scipy.signal
 import h5py
 
-ModelFolder="D:\CowBhave\Models"
+ProjectFolder="D:/CowBhave1/"
+ModelFolder=ProjectFolder+"Models25"
 WindowSizeList=[5,10,20,30,60,90,120,180,300] #sec
-# WindowSizeList=[20] #sec
+# WindowSizeList=[60] #sec
 Freq=25 #Hz
 FeedingM, RuminatingM, NothingM, DrinkingM = 1, 2, 3, 4
-FoldN=10
 
-TrainingDataFolder,TrainingDataSetName=[],[]
-# TrainingDataFolder.append("D:\\CowBhave\\Data_Exp08_11_2019\\Labeled"),TrainingDataSetName.append("Barn1")
-TrainingDataFolder.append("D:\\CowBhave\\Data_Exp15_02_2021\\Labeled"),TrainingDataSetName.append("Barn2")
-# TrainingDataFolder.append("D:\\CowBhave\\Mix12"),TrainingDataSetName.append("Mix12")
-# TrainingDataFolder.append("D:\\CowBhave\\Pavlovic21\\Labeled"),TrainingDataSetName.append("Barn3")
-# TrainingDataFolder.append("D:\\CowBhave\\MixA"),TrainingDataSetName.append("MixA")
+DataSetFolder,DataSetName=[],[]
+DataSetFolder.append(ProjectFolder+"Labeled25"),DataSetName.append("Barn2")
+# DataSetFolder.append(ProjectFolder+"Pavlovic21/Labeled25"),DataSetName.append("BarnP")
+# DataSetFolder.append(ProjectFolder+"Labeled25_10"),DataSetName.append("Barn2_10")
+# DataSetFolder.append(ProjectFolder+"Labeled25_20"),DataSetName.append("Barn2_20")
+# DataSetFolder.append(ProjectFolder+"Labeled25_30"),DataSetName.append("Barn2_30")
+# DataSetFolder.append(ProjectFolder+"Labeled25_40"),DataSetName.append("Barn2_40")
+# DataSetFolder.append(ProjectFolder+"Labeled25_50"),DataSetName.append("Barn2_50")
+# DataSetFolder.append(ProjectFolder+"Labeled25_60"),DataSetName.append("Barn2_60")
+# DataSetFolder.append(ProjectFolder+"Labeled25_70"),DataSetName.append("Barn2_70")
+# DataSetFolder.append(ProjectFolder+"Labeled25_80"),DataSetName.append("Barn2_80")
+# DataSetFolder.append(ProjectFolder+"Labeled25_90"),DataSetName.append("Barn2_90")
+# DataSetFolder.append(ProjectFolder+"Labeled25_60B1"),DataSetName.append("Barn2_60B1")
+# DataSetFolder.append(ProjectFolder+"Labeled25_60B2"),DataSetName.append("Barn2_60B2")
+# DataSetFolder.append(ProjectFolder+"Labeled25_60B3"),DataSetName.append("Barn2_60B3")
+# DataSetFolder.append(ProjectFolder+"Labeled25_60B4"),DataSetName.append("Barn2_60B4")
+# DataSetFolder.append(ProjectFolder+"Labeled25_60B5"),DataSetName.append("Barn2_60B5")
+# DataSetFolder.append(ProjectFolder+"Labeled25_60B6"),DataSetName.append("Barn2_60B6")
+# DataSetFolder.append(ProjectFolder+"Labeled25_60B7"),DataSetName.append("Barn2_60B7")
+# DataSetFolder.append(ProjectFolder+"Labeled25_60B8"),DataSetName.append("Barn2_60B8")
+# DataSetFolder.append(ProjectFolder+"Labeled25_60B9"),DataSetName.append("Barn2_60B9")
+# DataSetFolder.append(ProjectFolder+"Labeled25_60B10"),DataSetName.append("Barn2_60B10")
+FoldN=10
+ModelType="CNN2"
+PretrainedModelName=""#"BarnP"#
+FrozenLayersN=1
 
 Vers_i=0
-ModelName="NN1"#"RF1"#"SVM1"#
 for WindowSize in WindowSizeList:
-    # WindowN=int(WindowSize*Freq)
-    for TrainingDataSet_i in range(len(TrainingDataSetName)):
-        for Fold_i in range(FoldN):
-            # print(ModelName + ", WS=" + str(WindowSize) + ", " + TrainingDataSetName[TrainingDataSet_i] + ", " + TrainingDataSetName[TrainingDataSet_i] +", Fold "+str(Fold_i))
-            if ModelName in ["NN1", "NN2", "NN3", "NN4"]:
-                DataFileName=TrainingDataFolder[TrainingDataSet_i]+"\\"+'FeedingBehaviour_Validation'+"_WS"+str(WindowSize)+"_F"+str(Freq)+"_Fold"+str(Fold_i+1)+'.h5'
-                print(DataFileName)
-                f = h5py.File(DataFileName, 'r')
-                ASlicedValidation = f['AccXYZ'][...]
-                LabelSlicedValidation = f['Label'][...]
-                f.close()
-                model=tensorflow.keras.models.load_model(ModelFolder+"\\"+ModelName+TrainingDataSetName[TrainingDataSet_i]+"WS"+str(WindowSize)+"Fold"+str(Fold_i+1)+"V"+str(Vers_i))
+    for DataSetFolder_i in range(len(DataSetFolder)):
+        for Fold_i in range(FoldN+1):
+            if Fold_i==0:
+                DataFileName=DataSetFolder[DataSetFolder_i]+"/"+'FeedingBehaviour_Validation'+"_WS"+str(WindowSize)+"_F"+str(Freq)+'.h5'
+                if PretrainedModelName=="":
+                    ModelFileName=ModelFolder+"/"+ModelType+DataSetName[DataSetFolder_i]+"WS"+str(WindowSize)+"Fold"+str(0)+"V"+str(Vers_i)
+                else:
+                    ModelFileName=ModelFolder+"/"+ModelType+PretrainedModelName+"WS"+str(WindowSize)+"Fold"+str(0)+"V"+str(Vers_i)+"_TL"+DataSetName[DataSetFolder_i]+"_L"+str(FrozenLayersN)+"Fold"+str(0)+"V"+str(Vers_i)
+            else:
+                DataFileName=DataSetFolder[DataSetFolder_i]+"/"+'FeedingBehaviour_Validation'+"_WS"+str(WindowSize)+"_F"+str(Freq)+"_Fold"+str(Fold_i)+'.h5'
+                if PretrainedModelName=="":
+                    ModelFileName=ModelFolder+"/"+ModelType+DataSetName[DataSetFolder_i]+"WS"+str(WindowSize)+"Fold"+str(Fold_i)+"V"+str(Vers_i)
+                else:
+                    ModelFileName=ModelFolder+"/"+ModelType+PretrainedModelName+"WS"+str(WindowSize)+"Fold"+str(0)+"V"+str(Vers_i)+"_TL"+DataSetName[DataSetFolder_i]+"_L"+str(FrozenLayersN)+"Fold"+str(Fold_i)+"V"+str(Vers_i)
 
-                FeedingBehaviorPredictedP=model.predict(ASlicedValidation)
-                FeedingBehaviorPredicted=list()
-                for i in range(len(FeedingBehaviorPredictedP)):
-                    m,k=MaxInd(FeedingBehaviorPredictedP[i])
-                    FeedingBehaviorPredicted.append(k+1)
+            print(DataFileName)
+            f = h5py.File(DataFileName, 'r')
+            ASlicedValidation = f['AccXYZ'][...]
+            LabelSlicedValidation = f['Label'][...]
+            f.close()
 
-            elif ModelName=="RF1" or ModelName=="SVM1":
-                DataFileName=TrainingDataFolder[TrainingDataSet_i]+"\\"+"FeedingBehaviour_FeaturesValidation_WS"+str(WindowSize)+"_F"+str(Freq)+'.h5'
-                f = h5py.File(DataFileName, 'r')
-                print(DataFileName)
-                ASlicedFeaturesValidation = f['AccXYZFeatures'][...]
-                LabelSlicedValidation = f['Label'][...]
-                f.close()
+            print(ModelFileName)
+            model=tensorflow.keras.models.load_model(ModelFileName)
 
-                # WindowN=int(WindowSize*Freq)
-                # TagNo, CowNo, TimeStamp, Ax, Ay, Az, Label=ReadLabeledDataFiles(ValidationDataFolder[ValidationDataSet_i],'',Freq)
-                # AxSlicedT, AySlicedT, AzSlicedT, LabelSlicedT, TagNoSlicedT, CowNoSlicedT, TimeStampSlicedT=LabeledDataSlicing(Ax, Ay, Az, Label, WindowN, 0.5, TagNo, CowNo, TimeStamp)
-                # AxSlicedT=numpy.squeeze(AxSlicedT)
-                # AySlicedT=numpy.squeeze(AySlicedT)
-                # AzSlicedT=numpy.squeeze(AzSlicedT)
-                # LabelSlicedT=numpy.squeeze(LabelSlicedT)
-
-                # f,l=AccelerationFeatures(AxSlicedT[0],AySlicedT[0],AzSlicedT[0],LabelSlicedT[0],Freq)
-                # ASlicedFeaturesValidation=f
-                # LabelSlicedValidation=l
-                # for i in range(len(AxSlicedT)):
-                #     f,l=AccelerationFeatures(AxSlicedT[i],AySlicedT[i],AzSlicedT[i],LabelSlicedT[i],Freq)
-                #     ASlicedFeaturesValidation=numpy.vstack((ASlicedFeaturesValidation,f))
-                #     LabelSlicedValidation=numpy.vstack((LabelSlicedValidation,l))
-
-                # LabelSlicedValidation=numpy.squeeze(LabelSlicedValidation)
-
-            # print(ASlicedFeaturesValidation)
-            # print(LabelSlicedValidation)
-            # print(ASlicedFeaturesValidation.shape)
-            # print(LabelSlicedValidation.shape)
-
-
-                clf = joblib.load(ModelFolder+"\\"+ModelName+TrainingDataSetName[TrainingDataSet_i]+"WS"+str(WindowSize)+"Fold"+str(Fold_i)+"V"+str(Vers_i)+".joblib")
-                FeedingBehaviorPredicted=clf.predict(ASlicedFeaturesValidation)+1*0    
+            FeedingBehaviorPredictedP=model.predict(ASlicedValidation)
+            FeedingBehaviorPredicted=list()
+            for i in range(len(FeedingBehaviorPredictedP)):
+                m,k=MaxInd(FeedingBehaviorPredictedP[i])
+                FeedingBehaviorPredicted.append(k+1)
 
             FeedingBehaviorPredicted=scipy.signal.medfilt(FeedingBehaviorPredicted,5)
             PerfVect, ConfusionMatr, TotAcc=PresentPerformance(LabelSlicedValidation,FeedingBehaviorPredicted,[FeedingM, RuminatingM, NothingM],["Feeding", "Ruminating", "Nothing"])
             now = datetime.now()
             dt_string = now.strftime("%Y%m%d_%H%M%S")
-            s=dt_string+";"+ModelName+";"+TrainingDataSetName[TrainingDataSet_i]+";"+TrainingDataSetName[TrainingDataSet_i]+";"+str(WindowSize)+";"+str(Fold_i+1)+";"+str(Vers_i)+";"+str(TotAcc)+";"
+            if PretrainedModelName=="":
+                s=dt_string+";"+ModelType+";"+DataSetName[DataSetFolder_i]+";"+DataSetName[DataSetFolder_i]+";"+str(WindowSize)+";"+str(Fold_i)+";"+str(Vers_i)+";"+str(TotAcc)+";"
+            else:
+                s=dt_string+";"+ModelType+";"+PretrainedModelName+"L"+str(FrozenLayersN)+"TL"+DataSetName[DataSetFolder_i]+";"+DataSetName[DataSetFolder_i]+";"+str(WindowSize)+";"+str(Fold_i)+";"+str(Vers_i)+";"+str(TotAcc)+";"
             for i in range(len(ConfusionMatr)):
                 for j in range(len(ConfusionMatr)):
                     s=s+str(ConfusionMatr[i,j])+";"
-            fileH=open(ModelFolder+"\\"+'Performance.csv', 'a')
+            fileH=open(ModelFolder+"/"+'Performance.csv', 'a')
             fileH.write(s+'\n')
             fileH.close()
 
